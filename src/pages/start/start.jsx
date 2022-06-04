fe450c7d45af5755225c99131da89962d95a7377
Jakob Diedrich <93330435+zuehler@users.noreply.github.com>
2022-06-04 18:30:58 +0200

    Scale pics and repair navbar (#6)
    * pics scaled and camelizer tool added
    
    * navbar repaired
    
    * landing page adjusted

--- src/pages/start/start.jsx
+++ src/pages/start/start.jsx
@@ -1,8 +1,8 @@
 export const Start = () => {
   return (
     <div className="start">
-      <h1>Hi, we are SOKO_RAVE.</h1>
-      <h1>Wann haben wir uns zuletzt gesehen?</h1>
+      <h1>Welcome, we are SOKO_RAVE.</h1>
+      <h2>Hi. Wann haben wir uns zuletzt gesehen?</h2>
     </div>
   );
 };
