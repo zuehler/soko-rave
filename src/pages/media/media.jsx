b215b4f3678ec9b10b8415d326f5a50c77b7b728
zuehler <jakob.diedrich@gmx.de>
2022-06-08 13:23:07 +0200

    started adding media subpage structure, switch to new branch now to continue

--- src/pages/media/media.jsx
+++ src/pages/media/media.jsx
@@ -1,7 +1,16 @@
+import sets from "../../data/sets.json";
+
 export const Media = () => {
   return (
     <div className="media">
-      <h1>Media</h1>
+      <h1>SOKO_RAVE | records</h1>
+      <div className="sets">
+        <ul>
+          {sets.map((set, index) => {
+            return <li key={index}>{set.members.join(" / ")}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
