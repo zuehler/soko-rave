58d08942ad9bd08ef53df2dcbddd678d40b007c0
zuehler <jakob.diedrich@gmx.de>
2022-06-08 19:28:29 +0200

    filled content database and media subpage

--- src/pages/media/media.jsx
+++ src/pages/media/media.jsx
@@ -1,11 +1,19 @@
 import sets from "../../data/sets.json";
 import "../../styles/subpage-media/media.scss";
 import ReactPlayer from "react-player";
+import { useState } from "react";
 
 export const Media = () => {
   return (
     <div className="media">
       <h1>SOKO_RAVE | records</h1>
+      <div className="searchBar">
+        <label>filter</label>
+        <input
+          type="text"
+          placeholder="e.g. Lomo, objekt klein a, Techno, Braunschweig ..."
+        />
+      </div>
       <div className="sets">
         <ul>
           {sets
@@ -16,11 +24,12 @@ export const Media = () => {
             .map((set, index) => {
               return (
                 <ul key={index}>
-                  <li>{set.members.join(" b2b ")}</li>
+                  <li>{set.artists.join(" b2b ")}</li>
+                  <li>{set.title}</li>
                   <li>
                     {set.date} - {set.location}
                   </li>
-                  <li>{set.title}</li>
+                  <li>{set.genre.join(" / ")}</li>
                   <li>
                     <ReactPlayer height="150px" url={set.link} />
                   </li>
