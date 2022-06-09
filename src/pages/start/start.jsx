082e6c0d5e82293526015c709949a67fa23366f2
zuehler <jakob.diedrich@gmx.de>
2022-06-09 10:44:32 +0200

    changed img path for netlify

--- src/pages/start/start.jsx
+++ src/pages/start/start.jsx
@@ -3,7 +3,7 @@ import "../../styles/subpage-start/start.scss";
 export const Start = () => {
   return (
     <div className="start">
-      <h1>SOKO_RAVE | welcome.</h1>
+      <h1>SOKO_RAVE | welcome</h1>
       <h2>Hi. Wann haben wir uns zuletzt gesehen?</h2>
     </div>
   );
